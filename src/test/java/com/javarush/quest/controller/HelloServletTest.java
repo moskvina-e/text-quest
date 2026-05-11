8903101677603b9418f8db2a28888a23862b2bea
Elena Moskvina <e.moskvina.jj@gmail.com>
2026-05-11 16:07:57 +0500

    add test for HelloServlet

---
+++ src/test/java/com/javarush/quest/controller/HelloServletTest.java
@@ -0,0 +1,47 @@
+package com.javarush.quest.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class HelloServletTest {
+
+    private HelloServlet servlet;
+
+    @Mock
+    HttpServletRequest req;
+    @Mock
+    HttpServletResponse resp;
+    @Mock
+    RequestDispatcher dispatcher;
+
+    @BeforeEach
+    void setUp() {
+        servlet = new HelloServlet();
+    }
+
+    @Test
+    @DisplayName("Test Hello servlet")
+    void testHelloServlet() throws ServletException, IOException {
+        when(req.getRequestDispatcher("/index.jsp")).thenReturn(dispatcher);
+        servlet.doGet(req, resp);
+        verify(req, times(1)).getRequestDispatcher("/index.jsp");
+        verify(dispatcher, times(1)).forward(req, resp);
+
+    }
+
+}
